a8e30dcf66af8dca0f3b5d2636ab7c6cc4b4e584
皇甫睿欣 <1197066121@qq.com>
2018-11-09 15:54:48 +0800

    Valid Parentheses

--- src/Solution14.java
+++ src/Solution14.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+/**
+ * Longest Common Prefix
+ */
 
 public class Solution14 {
+
+    private String longestOfTwo(String a, String b) {
+        int j;
+        int alen = a.length();
+        int blen = b.length();
+        int len = alen < blen ? alen : blen;
+        for(j=0;j<len;j++){
+            if(a.charAt(j)!=b.charAt(j)){
+                break;
+            }
+        }
+        return a.substring(0,j);
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0){
+            return "";
+        }
+        if(strs.length==1){
+            return strs[0];
+        }
+        StringBuilder pre = new StringBuilder(longestOfTwo(strs[0],strs[1]));
+        if(pre.length()==0)
+            return "";
+        for(int i=2;i<strs.length;i++){
+            pre = new StringBuilder(longestOfTwo(pre.toString(),strs[i]));
+            if(pre.length()==0){
+                break;
+            }
+        }
+        return pre.toString();
+    }
 }
---
+++ src/Solution20.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+/**
+ * Valid Parentheses
+ */
+public class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+}
