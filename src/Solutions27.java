f09173c1b99fa3a7ee180ab2b3b609a4ad70267e
皇甫睿欣 <1197066121@qq.com>
2018-11-13 00:08:24 +0800

    Remove Element

---
+++ src/Solutions27.java
@@ -0,0 +1,17 @@
+/**
+ * solution27 Remove Element
+ */
+public class Solutions27 {
+
+    public int removeElement(int[] nums, int val) {
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+        return pointer;
+    }
+
+}
