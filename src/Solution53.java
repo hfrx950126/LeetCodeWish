d84d927cc556ed4f05817ff46ccae28faef24ac7
皇甫睿欣 <1197066121@qq.com>
2018-11-17 17:03:54 +0800

    solution 53

---
+++ src/Solution53.java
@@ -0,0 +1,23 @@
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1){
+            return nums[0];
+        }
+        int max = nums[0];
+        int cur = 0;
+        for (int val : nums) {
+            cur +=val;
+            if (max < cur){
+                max = cur;
+            }
+            if (cur<0){
+                cur = 0;
+            }
+
+        }
+        return max;
+    }
+}
