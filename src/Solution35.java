4379386e0592e846c8518a83d9c6586542bba560
皇甫睿欣 <1197066121@qq.com>
2018-11-17 16:45:07 +0800

    solution35

---
+++ src/Solution35.java
@@ -0,0 +1,17 @@
+public class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+        int left = 0, right = nums.length - 1, mid;
+        while(left <= right) {
+            mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
