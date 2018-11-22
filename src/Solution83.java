c75098fecffa7d525067224521ed9cdaeb093182
huangfuruixin <huangfuruixin@cmcm.com>
2018-11-22 20:45:42 +0800

    solution 83

---
+++ src/Solution83.java
@@ -0,0 +1,25 @@
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
+
+public class Solution83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode list = head;
+
+        while(list != null) {
+            if (list.next == null) {
+                break;
+            }
+            if (list.val == list.next.val) {
+                list.next = list.next.next;
+            } else {
+                list = list.next;
+            }
+        }
+
+        return head;
+    }
+}
