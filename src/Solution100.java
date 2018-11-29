70a480918b80740cb98ffec9efc28ed3466357bb
huangfuruixin <huangfuruixin@cmcm.com>
2018-11-29 10:53:00 +0800

    solution 100

---
+++ src/Solution100.java
@@ -0,0 +1,24 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+        }
+        return false;
+    }
+
+}
