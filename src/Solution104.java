7490bc43a8e0609d9c0574938673478c3a215e87
huangfuruixin <huangfuruixin@cmcm.com>
2018-11-29 14:15:03 +0800

    solution104

---
+++ src/Solution104.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * BFS
+ */
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+}
