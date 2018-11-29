7ee026bc75ce2ca7afc42ed935b777c6b8106dc7
huangfuruixin <huangfuruixin@cmcm.com>
2018-11-29 16:43:08 +0800

    solution111

--- src/Solution110.java
+++ src/Solution110.java
@@ -16,6 +16,31 @@ public class Solution110 {
         return 1 + Math.max(l, r);
     }
 
+    public int maxLeftDepth(TreeNode root){
+        if (root == null)
+            return 0;
+        int l = maxLeftDepth(root.left);
+        int r = maxLeftDepth(root.right);
+        System.out.println("l="+l);
+        System.out.println("r="+r);
+        return 1 + Math.max(l, r);
+    }
 
+
+    public static void main(String args[]){
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.right = new TreeNode(6);
+        Solution110 solution110 = new Solution110();
+        System.out.println(solution110.maxLeftDepth(treeNode1));
+
+    }
 }
 
---
+++ src/Solution111.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution111 {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int depth = 1;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            // for each level
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null){
+                    return depth;
+                }
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
