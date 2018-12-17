d9dfa911675eee4dfe676c4a7289862300173350
huangfuruixin <huangfuruixin@cmcm.com>
2018-12-17 20:16:42 +0800

    链表中环的入口节点

---
+++ src/swordmeansoffer/Solution22.java
@@ -0,0 +1,27 @@
+package swordmeansoffer;
+
+/**
+ * 链表中环的入口节点
+ */
+public class Solution22 {
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null || pHead.next == null)
+            return null;
+        ListNode p1 = pHead;
+        ListNode p2 = pHead;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2) {
+                p1 = pHead;
+                while (p1 != p2) {
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                return p1;
+            }
+        }
+        return null;
+    }
+
+}
